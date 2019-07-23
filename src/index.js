2d2334583792084d8a16f4c1a7a9b51c972dc2f8
Sashank Gogula <t-sagogu@microsoft.com>
2019-07-23 12:17:50 -0700

    Added firebase

--- src/index.js
+++ src/index.js
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import firebase from 'firebase';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyB9ddeBgRtiM0qWYwRDrdjJ2oPkzy91obM",
+    authDomain: "bingo-5724e.firebaseapp.com",
+    databaseURL: "https://bingo-5724e.firebaseio.com",
+    projectId: "bingo-5724e",
+    storageBucket: "bingo-5724e.appspot.com",
+    messagingSenderId: "348168330140",
+    appId: "1:348168330140:web:53270948a2bb66bc"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+firebase.database().ref('test').set({
+    valueA: "hello", 
+    valueB: "world",
+})
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
