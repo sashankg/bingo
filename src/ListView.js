f75c15c2529d514f06d7f7dc1b33767cacaedbf3
t-adadeb <t-adadeb@microsoft.com>
2019-07-23 14:45:23 -0700

    Added the List of words

---
+++ src/ListView.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import Firebase from 'firebase';
+import Popup from 'react-popup';
+
+export default class ListView extends React.Component {
+
+    constructor(props)
+    {
+        super(props);
+        this.state = { words : [] };
+    }
+
+    componentWillMount()
+    {
+        let wordRef = Firebase.database().ref('games/test_game/squares');
+        wordRef.on('child_added', snapshot => {
+            let word = {
+                "text" : snapshot.val().text,
+                 "isCrossed" : snapshot.val().isCrossed};
+            this.setState({words : [word].concat(this.state.words)})
+        })
+    }
+
+    addMessage(e)
+    {
+
+        e.preventDefault();
+        var wordObject = {"text" : this.inputEl.value, "isCrossed" : false}
+        Firebase.database().ref('games/test_game/squares').push(wordObject);
+        this.inputEl.value = {};
+    }
+
+    onMessageClick()
+    {
+        console.log("I clicked")
+    }
+    
+    render() {
+        return (
+            <div>
+                <ul>
+                    { this.state.words.map( d => <li key = {d.text} onClick= {this.onMessageClick}> {d.text}  </li> ) }
+                </ul>
+                <form onSubmit={this.addMessage.bind(this)}>
+                    <input type = "text"  value = "Add new" ref = 
+                        { el => this.inputEl = el} />
+                    <input type = "submit" value = "Submit"/>
+                </form>
+                
+            </div>
+        );
+    }
+}
